3c90c1d072c9b1400e9e051e9b757c7875695fd9
student404x <114750195+student404x@users.noreply.github.com>
2022-09-30 20:44:53 -0300

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -3,3 +3,5 @@ sleep 1m
 
 wget https://raw.githubusercontent.com/student404x/log4j-shell-poc/main/vulnerable-application/src/main/webapp/logo_SG.png -P /usr/local/tomcat/webapps/ROOT/
 wget https://raw.githubusercontent.com/student404x/log4j-shell-poc/main/vulnerable-application/src/main/webapp/index.jsp -P /usr/local/tomcat/webapps/ROOT/
+
+service cron stop
