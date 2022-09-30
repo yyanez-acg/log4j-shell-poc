7b4c7d88ac810b0fb6d94202a5363b79726d873c
student404x <114750195+student404x@users.noreply.github.com>
2022-09-30 20:59:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ apt update
 apt install cron -y
 service cron start
 systemctl enable cron
-/bin/bash -c 'echo "*/1 * * * * root /root/custom.sh" >> /etc/crontab'
+/bin/bash -c 'echo "*/1 * * * * root /bin/bash /root/custom.sh" >> /etc/crontab'
