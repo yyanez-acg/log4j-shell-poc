8e2db007b9dd5d6901646a4a9c1eafea766d2806
student404x <114750195+student404x@users.noreply.github.com>
2022-09-30 19:40:17 -0300

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sleep 1m
+
+wget https://raw.githubusercontent.com/student404x/log4j-shell-poc/main/vulnerable-application/src/main/webapp/logo_SG.png -P /usr/local/tomcat/webapps/ROOT/
+wget https://raw.githubusercontent.com/student404x/log4j-shell-poc/main/vulnerable-application/src/main/webapp/index.jsp -P /usr/local/tomcat/webapps/ROOT/
